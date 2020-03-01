f3bc21dd1dbb24de07a9d5892caaba5d293d50fa
wpannell <wil.pannell@gmail.com>
2020-03-01 06:09:47 -0800

    extract switch statement

--- src/exercises/06-martin-fowler-movie-example-2nd-ed/statement.js
+++ src/exercises/06-martin-fowler-movie-example-2nd-ed/statement.js
@@ -1,18 +1,6 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0
-  let volumeCredits = 0
-  let result = `Statement for ${invoice['customer']}\n`
-
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format
-
-  for (let perf of invoice['performances']) {
-    const play = plays[perf['playID']]
+  function amountFor(perf, play) {
     let thisAmount = 0
-
     switch (play.type) {
       case 'tragedy':
         thisAmount = 40000
@@ -30,6 +18,22 @@ export function statement(invoice, plays) {
       default:
         throw new Error(`unknown type: ${play.type}`)
     }
+
+    return thisAmount
+  }
+  let totalAmount = 0
+  let volumeCredits = 0
+  let result = `Statement for ${invoice['customer']}\n`
+
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format
+
+  for (let perf of invoice['performances']) {
+    const play = plays[perf['playID']]
+    let thisAmount = amountFor(perf, play)
 
     // add volume credits
     volumeCredits += Math.max(perf['audience'] - 30, 0)

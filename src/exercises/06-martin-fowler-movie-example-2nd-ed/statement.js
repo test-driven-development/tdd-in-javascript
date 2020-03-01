a5e037cc1181aa7dfa927d1b9be0ce4ecec48f28
wpannell <wil.pannell@gmail.com>
2020-03-01 09:31:37 -0800

    calculate `play` from `performance` in `amountFor` using `playFor(performance)`

--- src/exercises/06-martin-fowler-movie-example-2nd-ed/statement.js
+++ src/exercises/06-martin-fowler-movie-example-2nd-ed/statement.js
@@ -5,7 +5,7 @@ export function statement(invoice, plays) {
 
   function amountFor(performance, play) {
     let result = 0
-    switch (play.type) {
+    switch (playFor(performance).type) {
       case 'tragedy':
         result = 40000
         if (performance['audience'] > 30) {
@@ -20,7 +20,7 @@ export function statement(invoice, plays) {
         result += 300 * performance['audience']
         break
       default:
-        throw new Error(`unknown type: ${play.type}`)
+        throw new Error(`unknown type: ${playFor(performance).type}`)
     }
 
     return result

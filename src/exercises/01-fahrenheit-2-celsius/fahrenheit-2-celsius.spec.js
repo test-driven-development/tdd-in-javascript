95194e3fee999f8bd54531f7ddce2088600448e5
Osman Sufi <osufi@ford.com>
2021-06-04 15:39:14 -0400

    Pass first test for converter

--- src/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ src/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,3 +1,18 @@
 describe('the canary spec', () => {
-  test.todo('shows the test infrastructure works')
+  test('shows the test infrastructure works', () => {
+    true.should.be.true()
+  })
+})
+
+function fahrenheit2celsius(number) {
+  return 0
+}
+
+describe('fahrenheit to celsius converter', () => {
+  test('fahrenheit2celsius(32) = 0', () => {
+    fahrenheit2celsius(32).should.equal(0)
+  })
+
+  test.todo('fahrenheit2celsius(212) = 100')
+  test.todo('fahrenheit2celsius(-459.67) = -273.15')
 })

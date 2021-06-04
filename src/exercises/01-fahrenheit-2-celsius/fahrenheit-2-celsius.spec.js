f3dd89e1660e00fb56d8688a2f66ddf72c33dc72
wpannel4 <wpannel4@ford.com>
2021-06-04 12:25:45 -0700

    design fahrenheit to celsius converter

--- src/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ src/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,3 +1,11 @@
 describe('the canary spec', () => {
-  test.todo('shows the test infrastructure works')
+  test('shows the test infrastructure works', () => {
+    true.should.be.true()
+  })
+})
+
+describe('fahrenheit to celsius converter', () => {
+  test.todo('fahrenheit2celsius(32) = 0')
+  test.todo('fahrenheit2celsius(212) = 100')
+  test.todo('fahrenheit2celsius(85) = 50')
 })

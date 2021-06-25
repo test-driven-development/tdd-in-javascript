ef8f27dcedd6198607cbcfd69cb7df4f9d9efa54
Andrew Erickson <aericks9@ford.com>
2021-06-25 14:34:34 -0500

    palindrom design

--- src/exercises/02-palindrome/palindrome.spec.js
+++ src/exercises/02-palindrome/palindrome.spec.js
@@ -1,3 +1,16 @@
-describe('the canary spec', () => {
-  test.todo('shows the test infrastructure works')
+describe(`the canary spec`, () => {
+  test.todo(`shows the test infrastructure works`)
+})
+
+describe(`palindrome identifier`, () => {
+  test.todo(`isPalindrome('kayak') = true`)
+  test.todo(`isPalindrome('Kayak') = true`)
+  test.todo(`isPalindrome('aWord') = false`)
+  test.todo(`isPalindrome('1abba1') = false`)
+  test.todo(`isPalindrome('?abba?') = false`)
+  test.todo(`isPalindrome('a') = true`)
+  test.todo(`isPalindrome('aaaa') = true`)
+  test.todo(`isPalindrome('Top spot') = true`)
+  test.todo(`isPalindrome(12345) causes error`)
+  test.todo(`isPalindrome('') causes error`)
 })

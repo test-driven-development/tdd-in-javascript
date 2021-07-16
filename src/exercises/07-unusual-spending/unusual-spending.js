7d1d6576b9a318449492edc00c798f2e857767b4
Osman Sufi <osufi@ford.com>
2021-07-16 14:41:18 -0400

    mob next [ci-skip] [ci skip] [skip ci]

--- src/exercises/07-unusual-spending/unusual-spending.js
+++ src/exercises/07-unusual-spending/unusual-spending.js
@@ -5,5 +5,5 @@ import {email} from './email'
 export const unusualSpending = userId => {
   const payments = fetch(userId)
   const categorizedPayments = categorize(payments)
-
+  email(userId, categorizedPayments)
 }

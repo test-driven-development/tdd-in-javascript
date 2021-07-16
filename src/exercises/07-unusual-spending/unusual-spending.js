f5ce72442143078d8942440cfd60dba71adc3698
kharr201 <kharr201@ford.com>
2021-07-16 14:36:35 -0400

    mob next [ci-skip] [ci skip] [skip ci]

--- src/exercises/07-unusual-spending/unusual-spending.js
+++ src/exercises/07-unusual-spending/unusual-spending.js
@@ -1,1 +1,9 @@
-export const unusualSpending = () => {}
+import {fetch} from './fetch'
+import {categorize} from './categorize'
+import {email} from './email'
+
+export const unusualSpending = userId => {
+  const payments = fetch(userId)
+  const categorizedPayments = categorize(payments)
+
+}

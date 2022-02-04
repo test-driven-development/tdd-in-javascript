49468b23d70d89bef5ca7866317623270f818e01
jkarevic@ford.com <jkarevic@MGC12FD61QMD6N.fbpld77.ford.com>
2022-02-04 15:06:08 -0500

    implement first test of 32°f to 0°c

--- src/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ src/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,3 +1,9 @@
-describe('the canary spec', () => {
-  test.todo('shows the test infrastructure works')
+const fahrenheitToCelsius = () => 0
+
+describe('the fahrenheit to celsius converter', () => {
+  test('converts 32° fahrenheit to 0° celsius', () => {
+    fahrenheitToCelsius(32).should.equal(0)
+  })
+
+  test.todo('converts 212° fahrenheit to 100° celsius')
 })

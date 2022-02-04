76fa88c34b935549f26780100c920968f807d753
Osman Sufi <osufi@ford.com>
2022-02-04 15:55:25 -0500

    add test to convert 212 f to 100 c

--- src/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ src/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,8 +1,11 @@
-const fahrenheitToCelsius = () => 0
+const fahrenheitToCelsius = (temp) =>
+  (temp - 32) * 5/9
 
 describe('the fahrenheit to celsius converter', () => {
   test('converts 32° fahrenheit to 0° celsius', () => {
     fahrenheitToCelsius(32).should.equal(0)
   })
-  test.todo('converts 212° fahrenheit to 100° celsius')
+  test('converts 212° fahrenheit to 100° celsius', () => {
+    fahrenheitToCelsius(212).should.equal(100)
+  })
 })

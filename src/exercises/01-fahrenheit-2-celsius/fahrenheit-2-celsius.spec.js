5819bfb0daaadbfc333233b80fc8687453f1fc2b
Jabessa <gjabessa@MGC12DQ12GMD6N.fbpld77.ford.com>
2022-02-04 16:01:14 -0500

    write only enough code to change the console message

--- src/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ src/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,6 +1,10 @@
-let fahrenheitToCelsius = temp => 0
+let fahrenheitToCelsius = temp => ((temp - 32) * 5) / 9
 describe('the fahrenheit to celsius converter', () => {
   test('converts 32 to 0 celsius', () => {
     fahrenheitToCelsius(32).should.equal(0)
+  })
+
+  test('converts 212℉ to celsius degree', () => {
+    fahrenheitToCelsius(212).should.equal(100)
   })
 })

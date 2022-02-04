9c752b8d81ffd48f4ecb8976fc36291194feac47
sennali@ford.com <sennali@ford.com>
2022-02-04 15:44:03 -0500

    update function name

--- src/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ src/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,8 +1,8 @@
-const fahrenheitTocelsius = () => 0
+const fahrenheitToCelsius = () => 0
 
 describe('The fahrenheit to celsius converter', () => {
   test('converts 32° fahrenheit to 0° celsius', () => {
-    fahrenheitTocelsius(32).should.equal(0)
+    fahrenheitToCelsius(32).should.equal(0)
   })
   test.todo('converts 212° fahrenheit to 100° celsius')
 })

1d333429d4f05b2722a7f9b9fad96f404f6e46d7
wpannel4 <wpannel4@ford.com>
2022-02-11 11:38:39 -0800

    checkpoint for workshop

--- src/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ src/exercises/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -1,3 +1,12 @@
-describe('the canary spec', () => {
-  test.todo('shows the test infrastructure works')
+const fahrenheitToCelsius = fahrenheit =>
+  ((fahrenheit - 32) * 5) / 9
+
+describe('the fahrenheit to celsius converter', () => {
+  test('converts 32° fahrenheit to 0° celsius', () => {
+    fahrenheitToCelsius(32).should.equal(0)
+  })
+
+  test('converts 212° fahrenheit to 100° celsius', () => {
+    fahrenheitToCelsius(212).should.equal(100)
+  })
 })

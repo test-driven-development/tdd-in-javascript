77f19c487a012f90b9ad5942a67750067ed50610
jkarevic@ford.com <jkarevic@MGC12FD61QMD6N.fbpld77.ford.com>
2022-02-25 16:53:39 -0500

    Complete palindrome test suite implementation

--- src/exercises/02-palindrome/palindrome.spec.js
+++ src/exercises/02-palindrome/palindrome.spec.js
@@ -1,36 +1,84 @@
-let isPalindrome = phrase => {
-  const isOneWord = phrase.split(' ').length === 1
+function isOneWord(phrase) {
+  return phrase.split(' ').length === 1
+}
 
-  const isTheSameForwardAndBackward =
+function isTheSameForwardAndBackward(phrase) {
+  return (
     phrase ===
     phrase
       .split('')
       .reverse()
       .join('')
-
-  return isTheSameForwardAndBackward && isOneWord
+  )
 }
 
-describe(`the isPalindrome function`, () => {
-  test(`returns "true" for a "level"`, () => {
+function isArray(phrase) {
+  return phrase instanceof Array
+}
+
+function isString(phrase) {
+  return typeof phrase === 'string'
+}
+
+function isObject(phrase) {
+  return typeof phrase === 'object'
+}
+
+function isPalindrome(phrase) {
+  if (phrase === null) {
+    throw new Error('Phrase cannot be null')
+  }
+
+  if (isArray(phrase)) {
+    throw new Error('Phrase cannot be an array')
+  }
+
+  if (isObject(phrase)) {
+    throw new Error('Phrase cannot be an object')
+  }
+
+  return (
+    isString(phrase) &&
+    isTheSameForwardAndBackward(phrase) &&
+    isOneWord(phrase)
+  )
+}
+
+describe(`the isPalindrome function returns`, () => {
+  test(`"true" for "level"`, () => {
     isPalindrome('level').should.be.true()
   })
-  test(`returns "false" for a "lover"`, () => {
+
+  test(`"false" for "lover"`, () => {
     isPalindrome('lover').should.be.false()
   })
-  test(`returns "false" for a "heh heh"`, () => {
+
+  test(`"false" for "heh heh"`, () => {
     isPalindrome('heh heh').should.be.false()
   })
-  test.todo(`returns "true" for a "a"`)
-  test.todo(`returns "true" for a "e"`)
-  test.todo(`returns "false" for a 1`)
-  test.todo(`returns "false" for a 0`)
-  test.todo(`returns "false" for a 9`)
-  test.todo(`returns "false" for a 1628`)
-  test.todo(`returns "true" for a " "`)
-  test.todo(`returns "true" for a "   "`)
-  test.todo(`returns "true" for a ""`)
-  test.todo(`throw for a []`)
-  test.todo(`throw for a {}`)
-  test.todo(`throw for a null`)
+
+  test(`"true" for a single character like "a"`, () => {
+    isPalindrome('a').should.be.true()
+  })
+
+  test(`"false" for 1`, () => {
+    isPalindrome(1).should.be.false()
+  })
+
+  test(`throw for []`, () => {
+    ;(() => {
+      isPalindrome([])
+    }).should.throw('Phrase cannot be an array')
+  })
+
+  test(`throw for {}`, () => {
+    ;(() => {
+      isPalindrome({})
+    }).should.throw('Phrase cannot be an object')
+  })
+  test(`throw for null`, () => {
+    ;(() => {
+      isPalindrome(null)
+    }).should.throw('Phrase cannot be null')
+  })
 })
